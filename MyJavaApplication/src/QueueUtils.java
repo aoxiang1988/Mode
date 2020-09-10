6d570146c31b5fe4e9bedf68ba7c901d86851e95
aoxiang1988 <36103300+aoxiang1988@users.noreply.github.com>
2020-09-10 19:37:39 +0800

    Update QueueUtils.java
    change the init and function

--- MyJavaApplication/src/QueueUtils.java
+++ MyJavaApplication/src/QueueUtils.java
@@ -9,41 +9,78 @@ public class QueueUtils {
         return mQueueUtils;
     }
 
+    static class Queue {
+        QueueNode headNode;
+        QueueNode rearNode;
+        QueueNode linkQueue;
+
+        Queue(QueueNode linkQueue) {
+            this.headNode = linkQueue;
+            this.rearNode = linkQueue;
+            this.linkQueue = linkQueue;
+        }
+
+        QueueNode getHeadNode() {
+            return headNode;
+        }
+
+        QueueNode getLinkQueue() {
+            return linkQueue;
+        }
+
+        QueueNode getRearNode() {
+            return rearNode;
+        }
+
+        void setHeadNode(QueueNode headNode) {
+            this.headNode = headNode;
+        }
+
+        void setRearNode(QueueNode rearNode) {
+            this.rearNode = rearNode;
+        }
+
+        void setLinkQueue(QueueNode linkQueue) {
+            this.linkQueue = linkQueue;
+        }
+    }
+
     static class QueueNode {
         QueueNode next;
         Object value;
     }
 
-    private QueueNode mHeadNode = null;
-    private QueueNode mRearNode = null;
-    private QueueNode mLinkQueue = null;
+    //private QueueNode mHeadNode = null;
+    //private QueueNode mRearNode = null;
+    //private QueueNode mLinkQueue = null;
 
-    void initLinkQueue(Object v) {
-        mLinkQueue = new QueueNode();
-        mLinkQueue.next = null;
-        mLinkQueue.value = v;
-
-        mHeadNode = mLinkQueue;
-        mRearNode = mLinkQueue;
+    Queue initLinkQueue(Object v) {
+        QueueNode linkQueue = new QueueNode();
+        linkQueue.next = null;
+        linkQueue.value = v;
+        return new Queue(linkQueue);
     }
 
-    void pushToQueue(Object v) {
-        mRearNode.next = new QueueNode();
-        mRearNode.next.value = v;
-        mRearNode.next.next = null;
-        mRearNode = mRearNode.next;
+    void pushToQueue(Queue queue, Object v) {
+        QueueNode rearNode = queue.getRearNode();
+        rearNode.next = new QueueNode();
+        rearNode.next.value = v;
+        rearNode.next.next = null;
+        queue.setRearNode(rearNode.next);
     }
 
-    QueueNode popQueue() {
+    QueueNode popQueue(Queue queue) {
         QueueNode result = new QueueNode();
+        QueueNode mHeadNode = queue.getHeadNode();
         if (mHeadNode != null) {
             result.next = null;
             result.value = mHeadNode.value;
-            mHeadNode = mHeadNode.next;
+            queue.setHeadNode(mHeadNode.next);
 
-            QueueNode queue = mLinkQueue.next;
+            QueueNode mLinkQueue = queue.getLinkQueue();
+            QueueNode queueNode = mLinkQueue.next;
             mLinkQueue.next = null;
-            mLinkQueue = queue;
+            queue.setLinkQueue(queueNode);
         } else {
             return null;
         }
