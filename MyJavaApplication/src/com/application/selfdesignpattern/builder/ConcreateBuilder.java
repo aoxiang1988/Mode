08964cafab78972ccdbf33942f04efe9b4beacfb
aoxiang1988 <36103300+aoxiang1988@users.noreply.github.com>
2020-10-23 09:39:01 +0800

    Update ConcreateBuilder.java

--- MyJavaApplication/src/com/application/selfdesignpattern/builder/ConcreateBuilder.java
+++ MyJavaApplication/src/com/application/selfdesignpattern/builder/ConcreateBuilder.java
@@ -1,13 +1,33 @@
 package com.application.selfdesignpattern.builder;
 
 public class ConcreateBuilder extends Builder {
+
+    public static ConcreateBuilder Builder = new ConcreateBuilder();
+
     @Override
     public void buildPart() {
         getResult();
         getResult1();
     }
+
+    private String mProductName;
+
+    @Override
+    public Builder setProductName(String productName) {
+        mProductName = productName;
+        return Builder;
+    }
+
+    private int mValue;
+
+    @Override
+    public Builder setProductValue(int value) {
+        mValue = value;
+        return Builder;
+    }
+
     private Product getResult() {
-        return new Product();
+        return new Product(mProductName, mValue);
     }
 
     private Product1 getResult1() {
