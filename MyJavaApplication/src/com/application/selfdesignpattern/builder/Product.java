d22a7bb8b7201e73d9892a4e42afaa76385721ab
aoxiang1988 <36103300+aoxiang1988@users.noreply.github.com>
2020-10-23 09:41:05 +0800

    Update Product.java

--- MyJavaApplication/src/com/application/selfdesignpattern/builder/Product.java
+++ MyJavaApplication/src/com/application/selfdesignpattern/builder/Product.java
@@ -3,7 +3,8 @@ package com.application.selfdesignpattern.builder;
 import com.application.Log;
 
 public class Product {
-    public Product() {
-        Log.d(getClass().getName(), "I am product\n");
+    public Product(String name, int val) {
+        Log.i(getClass().getName(), "I am product " + name + "\n");
+        Log.i(getClass().getName(), "I am value " + val + "\n");
     }
 }
