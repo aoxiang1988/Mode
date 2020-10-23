ade322f2139cffbdf278a99c90dc1a9710004fd5
aoxiang1988 <36103300+aoxiang1988@users.noreply.github.com>
2020-10-23 09:38:03 +0800

    Update Builder.java

--- MyJavaApplication/src/com/application/selfdesignpattern/builder/Builder.java
+++ MyJavaApplication/src/com/application/selfdesignpattern/builder/Builder.java
@@ -2,4 +2,6 @@ package com.application.selfdesignpattern.builder;
 
 public abstract class Builder {
     public abstract void buildPart();
+    public abstract Builder setProductName(String productName);
+    public abstract Builder setProductValue(int value);
 }
