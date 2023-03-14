ac6d26707c73e7ba199b0f5981981f391eee3e6d
yangbin <yangbin_1111@126.com>
2023-03-14 10:21:42 +0800

    update

--- MyJavaApplication/Module/src/com/module/Main.java
+++ MyJavaApplication/Module/src/com/module/Main.java
@@ -2,11 +2,16 @@ package com.module;
 
 import com.module.difffilecheck.TagInfo;
 import com.module.difffilecheck.TagInfoListUtil;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiClient;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     /**
@@ -25,7 +30,96 @@ public class Main {
     public static void main(String[] args) {
 
         mUtil = new TagInfoListUtil();
+        //buildDiffFile();
+        diffInitFun ();
+    }
 
+    /**
+     *生成diff文件
+     * */
+    private static void buildDiffFile() {
+
+        InputInfoUtils mInputUtils = new InputInfoUtils();
+        System.out.print("please input manifest file path :> ");
+        String diffFilePath = mInputUtils.putFilePath();
+        File file = new File(diffFilePath);
+        List<String> diffFileList = new ArrayList<>();
+        if(file.isDirectory()) { // 判断File对象对应的目录是否存在
+            String[] names = file.list(); // 获得目录下的所有文件的文件名
+            for (String name : names) {
+                if (name.endsWith(".xml")) {
+                    //System.out.println(diffFilePath + "/" + name);
+                    diffFileList.add(diffFilePath + "\\" + name);
+                }
+            }
+
+            for (int n = 0; n<diffFileList.size(); n++) {
+                String command = "";
+                execute(command);
+            }
+
+        } else {
+            System.out.println("error!!!");
+        }
+    }
+
+    /**
+     * 调用ProcessBuilder执行，相比Runtime方式，返回值不易丢失
+     * @param command 命令
+     * @return 执行结果
+     */
+    private static String execute(String command) {
+        BufferedReader bufferedReader = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String result = null;
+        try {
+            File file = new File("C:\\daemonTmp");
+            // 新建一个存储结果的缓存文件
+            File tmpFile = new File("C:\\daemonTmp\\temp.tmp");
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            if (!tmpFile.exists()) {
+                tmpFile.createNewFile();
+            }
+            ProcessBuilder processBuilder = new ProcessBuilder()
+                    .command("cmd.exe", "/c", command).inheritIO();
+            // 把控制台中的红字变成了黑字，用通常的方法其实获取不到，控制台的结果是pb.start()方法内部输出的。
+            processBuilder.redirectErrorStream(true);
+            // 输出执行结果。
+            processBuilder.redirectOutput(tmpFile);
+            // 等待语句执行完成，否则可能会读不到结果。
+            processBuilder.start().waitFor();
+            InputStream inputStream = new FileInputStream(tmpFile);
+            //设置编码
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "GBK"));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+            bufferedReader.close();
+            bufferedReader = null;
+
+            result = stringBuilder.toString();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     *处理diff生成的文件
+     * */
+    private static void diffInitFun() {
         InputInfoUtils mInputUtils = new InputInfoUtils();
         System.out.print("please input Diff file path :> ");
         String diffFilePath = mInputUtils.putFilePath();
@@ -56,6 +150,10 @@ public class Main {
         }
     }
 
+
+    /**
+     * 生成patch 下载命令的批处理sh文件
+     * */
     private static void buildCmdFile(String diffFilePath, String patchCmdFile) {
         try {
             System.out.println("diff file path: "+diffFilePath);
